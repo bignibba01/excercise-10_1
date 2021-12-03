d67017cefe62fe8918a85f3a352cdee4d7a6eda7
bignibba01 <davidemarchetti01@gmail.com>
2021-12-03 12:17:50 +0100

    preparato un meno di scelta delle operazioni --problemi-- getNumberPlayer e con la lista

--- main/entity.h
+++ main/entity.h
@@ -7,9 +7,9 @@ struct Player {		//player entity
 	struct Player* next;
 };
 
-void popFirstElement(struct Player**);		//toglie un valore dalla testa della coda (FIFO)
-void printAllElement(struct Player*);		//visualizza tutta la coda
-void pushElement(struct Player*, char*);			//aggiunge un valore alla coda dei giocatori
+void popFirstElement(struct Player**);				//toglie un valore dalla testa della coda (FIFO)
+void printAllElement(struct Player*);				//visualizza tutta la coda
+void pushElement(struct Player*, int);			//aggiunge un valore alla coda dei giocatori
 
 void popFirstElement(struct Player** head) {
 	struct Player* next_node = NULL;
@@ -25,19 +25,19 @@ void popFirstElement(struct Player** head) {
 
 void printAllElement(struct Player* head) {
 	while (head != NULL) {
-		printf("%s\n", head->color);
+		printf("%d\n", head->id);
 		head = head->next;
 	}
 }
 
-void pushElement(struct Player* head, char color[20]) {
+void pushElement(struct Player* head, int id) {
 
 	while (head->next != NULL) {
 		head = head->next;
 	}
 
 	head->next = (struct Player*)malloc(sizeof(struct Player));
-	strcpy(head->next->color, color);
+	head->next->id = id;
 	head->next->next = NULL;
 }
 
--- main/main.c
+++ main/main.c
@@ -9,43 +9,109 @@ Il gioco consistente nel far competere più giocatori al raggiungimento della cas
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "entity.h"
 
 void drawMenu();							//disegna il menu principale
-void registerPlayer(struct Player*);		//aggiungi i giocatori al gioco
+int getNumberPlayer();	//probabile problema					//prende il numero dei giocatori
+void registerPlayer();						//inserisce il giocatore nella coda
+int rollDice();								//tira il dado (1-6)
+
+struct Player* head = NULL;
 
 int main() {
-	struct Player* head = NULL;
-
 
 	while (true) {
+		_Bool end = false;		//esce dal ciclo continuo
+		_Bool flag = true;		//esce dal ciclo dei controlli
+		int oper = 0;			//operazione scelta dall utente
 
-		char id[20];
-		printf("Add value: ");
-		scanf(" %[^\n]s", id);
-		fflush(stdin);
+		drawMenu();
+		do {
+			printf("Scegli operazione: ");
+			scanf(" %d", &oper);
+			fflush(stdin);
 
-		if(strcmp(id, "-1") != 0){
+			if (oper >= 1 && oper <= 4)
+				flag = false;
+			else
+				printf("Errore -> Operazione non valida\n");
+		} while (flag);
 
-			if (head == NULL) {			//se il primo elemento della lista è nullo allora inserisce il primo valore
-				head = (struct Player*)malloc(sizeof(struct Player));			//alloca lo spazio
-				strcpy(head->color, id);
-				head->next = NULL;			//attribuisce al puntatore all elemento successivo il valore NULL perche ancora non esiste
+		switch (oper) {
+		case 1: {
+			break;
 			}
-			else {		//se invece il primo elemento già esiste allora esegue un push
-				pushElement(head, id);
-			}
+		case 2: {
+			registerPlayer();
+			system("pause");
+			break;
 		}
-		else {
-			popFirstElement(&head);
+		case 3: {
+			printAllElement(head);
+			system("pause");
+			break;
 		}
-		printAllElement(head);
-		system("pause");
+		case 4: {
+			end = true;
+			break;
+		}
+		default: {
+			printf("Errore -> L'operazione non esiste\n");
+		}
+		}
+
+		if (end)
+			break;		//esce dal ciclo continuo
+
+		system("cls");
+		
 	}
 
 	return 0;
 }
 
 void drawMenu() {
+	printf("1)Inizia partita!\n2)Aggiungi giocatori (%d correnti)!\n3)Lista dei giocatori\n4)Esci\n", getNumberPlayer());
+}
 
+int getNumberPlayer() {
+	int tmpNumber = 0;
+
+	while (head != NULL) {
+		tmpNumber = head->id;
+		head = head->next;
+	}
+
+	return tmpNumber;
+}
+
+void registerPlayer() {
+
+	int tmpId = 0;
+	char tmpColor[20] = { 0 };
+
+	if (head == NULL) {			//se il primo elemento della lista è nullo allora inserisce il primo valore
+		head = (struct Player*)malloc(sizeof(struct Player));			//alloca lo spazio
+
+		head->id = 1;
+		head->next = NULL;			//attribuisce al puntatore all elemento successivo il valore NULL perche ancora non esiste
+	}
+	else {		//se invece il primo elemento già esiste allora esegue un push
+		struct Player* tmp = head;
+		while (tmp != NULL) {
+			tmpId = tmp->id + 1;			//preleva l'id dall ultimo elemento della lista e lo incrementa
+			tmp = tmp->next;
+		}
+		if (tmpId == 6)				//6 perche tmpId = tmp->id + 1 (5+1)
+			printf("Numero massimo di giocatori raggiunto!\n");
+		else
+			pushElement(head, tmpId);
+	}
+}
+
+int rollDice() {
+	srand(time(NULL));
+
+	return rand() % 6 + 1;
 }
